e58ee82c1c6f801502675bdb706872f09052b6bb
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2019-01-31 19:43:45 +0100

    chore: remove unused var

--- src/bomberman.py
+++ src/bomberman.py
@@ -18,16 +18,6 @@ SECTOR_SIZE = 16
 WALKING_SPEED = 5
 FLYING_SPEED = 15
 
-GRAVITY = 20.0
-MAX_JUMP_HEIGHT = 1.0 # About the height of a block.
-# To derive the formula for calculating jump speed, first solve
-#    v_t = v_0 + a * t
-# for the time at which you achieve maximum height, where a is the acceleration
-# due to gravity and v_t = 0. This gives:
-#    t = - v_0 / a
-# Use t and the desired MAX_JUMP_HEIGHT to solve for v_0 (jump speed) in
-#    s = s_0 + v_0 * t + (a * t^2) / 2
-JUMP_SPEED = math.sqrt(2 * GRAVITY * MAX_JUMP_HEIGHT)
 TERMINAL_VELOCITY = 50
 
 PLAYER_HEIGHT = 1
@@ -135,7 +125,7 @@ def sectorize(position):
     return x, 0, z
 
 
-def is_starting_position(x: int, z: int, field_size: int) -> bool:
+def is_starting_position(x, z, field_size):
     field_size = field_size / 2
 
     if x in (-field_size + 1, field_size - 1) and z in (-field_size + 1, field_size - 1):
