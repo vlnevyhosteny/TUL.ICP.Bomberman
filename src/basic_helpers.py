943fd4c470f8b135ecc25e3cd30089a5026c8348
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2019-02-05 19:38:25 +0100

    feat: split monolitic file to files

---
+++ src/basic_helpers.py
@@ -0,0 +1,110 @@
+import math
+from src.game_config import *
+
+
+def cube_vertices(x, y, z, n):
+    """ Return the vertices of the cube at position x, y, z with size 2*n.
+
+    """
+    return [
+        x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n,  # top
+        x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n,  # bottom
+        x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n,  # left
+        x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n,  # right
+        x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  # front
+        x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  # back
+    ]
+
+
+def tex_coord(x, y, n=4):
+    """ Return the bounding vertices of the texture square.
+
+    """
+    m = 1.0 / n
+    dx = x * m
+    dy = y * m
+    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
+
+def tex_coords(top, bottom, side):
+    """ Return a list of the texture squares for the top, bottom and side.
+
+    """
+    top = tex_coord(*top)
+    bottom = tex_coord(*bottom)
+    side = tex_coord(*side)
+    result = []
+    result.extend(top)
+    result.extend(bottom)
+    result.extend(side * 4)
+    return result
+
+def normalize(position):
+    """ Accepts `position` of arbitrary precision and returns the block
+    containing that position.
+
+    Parameters
+    ----------
+    position : tuple of len 3
+
+    Returns
+    -------
+    block_position : tuple of ints of len 3
+
+    """
+    x, y, z = position
+    x, y, z = (int(round(x)), int(round(y)), int(round(z)))
+    return x, y, z
+
+
+def sectorize(position):
+    """ Returns a tuple representing the sector for the given `position`.
+
+    Parameters
+    ----------
+    position : tuple of len 3
+
+    Returns
+    -------
+    sector : tuple of len 3
+
+    """
+    x, y, z = normalize(position)
+    x, y, z = x // SECTOR_SIZE, y // SECTOR_SIZE, z // SECTOR_SIZE
+    return x, 0, z
+
+
+def is_starting_position(x, z, field_size):
+    field_size = field_size / 2
+
+    if x in (-field_size + 1, field_size - 1) and z in (-field_size + 1, field_size - 1):
+        return True
+    if x in (-field_size + 2, field_size - 1) and z in (-field_size + 1, field_size - 1):
+        return True
+    if x in (-field_size + 1, field_size - 2) and z in (-field_size + 1, field_size - 1):
+        return True
+    if x in (-field_size + 1, field_size - 1) and z in (-field_size + 2, field_size - 1):
+        return True
+    if x in (-field_size + 1, field_size - 1) and z in (-field_size + 1, field_size - 2):
+        return True
+
+    return False
+
+
+def pythagoras_get_c(a, b):
+    return math.sqrt(a ^ 2 + b ^ 2)
+
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+
+    return qx, qy
---
+++ src/game_config.py
@@ -0,0 +1,31 @@
+TICKS_PER_SEC = 60
+
+# Size of sectors used to ease block loading.
+SECTOR_SIZE = 16
+
+WALKING_SPEED = 5
+FLYING_SPEED = 15
+
+TERMINAL_VELOCITY = 50
+
+PLAYER_HEIGHT = 1
+
+HALF_OF_FIELD_SIZE = 5
+
+STARTING_POSITION_X = 0
+STARTING_POSITION_Y = 8
+STARTING_POSITION_Z = -5
+
+STARTING_ROTATION_X = -180
+STARTING_ROTATION_Y = -65
+
+FACES = [
+    (0, 1, 0),
+    (0, -1, 0),
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+]
+
+TEXTURE_PATH = 'texture.png'
