f222683e4add63594caa50db20a0a0ba8b235121
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2020-06-09 18:03:40 +0200

    Revert "add simple lighting"
    This reverts commit fabc53fcb2440080e99946f62c7aa32d57ec7a5e.

--- src/game_config.py
+++ src/game_config.py
@@ -1,5 +1,3 @@
-from pyglet.gl import GLfloat
-
 TICKS_PER_SEC = 60
 
 # Size of sectors used to ease block loading.
@@ -20,9 +18,6 @@ STARTING_POSITION_Z = -5
 
 STATIC_LIGHT_POSITION = (20, 20, 20)
 
-SHADOW_WIDTH = 50
-SHADOW_HEIGHT = 20
-
 STARTING_ROTATION_X = -180
 STARTING_ROTATION_Y = -65
 
@@ -41,8 +36,3 @@ BOMB_STARTING_RANGE = 3
 BOMB_TIMESPAN_SECS = 3
 
 TRACING_GRASS_CONSTANT = 5
-
-ambientLight = (GLfloat * 4)(0.2, 0.2, 0.2, 1.0)
-diffuseLight = (GLfloat * 4)(0.7, 0.7, 0.7, 1.0)
-
-INITIAL_BOMBS_COUNT = 1
